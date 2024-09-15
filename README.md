f1ec127ec7f3c194b467b4b9b625dd2c8330635a
Jesus <113149095+jseusx@users.noreply.github.com>
2024-09-15 18:11:17 -0400

    Added files
    Contains files needed for this project.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# CSCI4120
-Machine Learning
+# Homework 1: Group Homework [iris_KNN]
+## Team Members:
+Jesus Jimenez-Sanchez <br> 
+**Email:** Jimenezsanchezj18@students.ecu.edu
+
+## Quick Start:
+Before running the program, make sure that the "iris.data" file is installed in the same directory.<br>
+Additionally, make sure that all libraries needed are installed from "requirements.txt". It includes these libraries:
+```
+scipy
+numpy
+pandas
+matplotlib
+seaborn
+scikit-learn
+pillow
+yellowbrick
+ipywidgets
+keras
+tensorflow
+```
+
+If you are on a linux device, use pip and use this command in the terminal to install them all:
+
+```console
+pip3 install -r requirements.txt
+```
+### Run the program:
+Using either terminal or an IDE to run the program. First, make sure that you cd into the directory where the **.py** file is contained.
+
+For example:
+**Terminal:**
+```console
+foo@bar:~$ cd Desktop/CSCI_4120-master/KNN/.
+foo@bar:~$ python3 'iris_KNN.py'
+...
+```
+You will then be shown a linechart similar to this and get to see the accuracies being displayed per k:
+![linechart plotting K values vs Average Accuracy](matplotlib_HW1.png)
+
+### Which K works best?
+According to the data shown, k=5 has the best accuracy. I have also noticed that when running the programming, usually k<5 are the highest, specifically k=3.
+My assumption with it being so accurate is that when using sklearn's library, the splitting of the data is more conistant. <br>
+KNeighborsClassifier may implement scaling if needed. While the original code assumed everything was on the same scale. 
+Overall, if the accuracies are similar across k's it may be due to the way the data is split and sorted when training and testing the AI. Due to the use of random_state it allows for reproducability.
+```python
+return train_test_split(x, y, test_size=1-split, random_state=42)
+```
+Additionally, having too many k's causes underfitting meaning it is not good on training data and cannot be used to predict the new data.
---
+++ iris_KNN.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+import random
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+def loadDataset(filename, split):
+    df = pd.read_csv(filename, header=None)
+    
+    array = df.to_numpy()
+    random.shuffle(array)
+ 
+    # gets everything before the last colomn
+    x = array[:, :-1]
+    # gets the last colomn
+    y = array[:, -1]
+    
+    # Split the dataset
+    # x_train, x_test, y_train, y_test
+    return train_test_split(x, y, test_size=1-split, random_state=42)
+    
+    
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x] == predictions[x]:
+            correct += 1
+    return (correct/float(len(testSet))) * 100.0
+    
+def main():
+    # prepare data
+    split = 0.67
+    url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
+    
+    k_values = range(1, 21)
+    num_repeats = 5
+
+    # List with accuracies depending on k
+    average_accuracies = []
+    
+    for k in k_values:
+        # place holder for 5 values to average
+        accuracies = []
+        for _ in range(num_repeats): 
+            trainingX, testX, trainingY, testY = loadDataset(url, split)	
+            
+            # trains the model
+            neigh = KNeighborsClassifier(n_neighbors=k)
+            neigh.fit(trainingX, trainingY)
+    
+            # predicts and gets accuracy per k
+            predictions = neigh.predict(testX)
+            accuracy = accuracy_score(testY, predictions)
+            # puts the accuracy into a list for that k
+            accuracies.append(accuracy)
+   
+        #finds current average ccuracy for k then appends it to overall list
+        avg_accuracy = np.mean(accuracies)
+        average_accuracies.append(avg_accuracy)
+
+        print(f'k={k} Average Accuracy: {avg_accuracy:.2f}%')
+    
+    
+    # chart setup
+    plt.style.use('seaborn-whitegrid')
+    plt.title('K values vs Average Accuracy')
+    plt.xlabel('K values')
+    plt.ylabel('Average Accuracy per K')
+    plt.xticks(k_values)
+    plt.grid(True)
+
+    plt.plot(k_values, average_accuracies, marker='o')
+
+    plt.show()
+
+    
+main()
