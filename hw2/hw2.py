2298e3d2b59edc262fdb4bd0cc5ac31033e31563
Jesus <113149095+jseusx@users.noreply.github.com>
2024-10-04 20:10:17 -0400

    Create hw2.py

---
+++ hw2/hw2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from yellowbrick.cluster import KElbowVisualizer
+
+
+X, y_true = make_blobs(n_samples=300, centers=4,
+                       cluster_std=0.60, random_state=0)
+
+#plt.scatter(X[:, 0], X[:, 1], s=50)
+# plt.show()
+
+model = KMeans()
+
+visualizer = KElbowVisualizer(model, k=(1,12))
+visualizer.fit(X)  # Fit the data to the visualizer
+visualizer.show()       
+
+#best k using KElbowVisualizer
+best_k = visualizer.elbow_value_ 
+
+kmeans = KMeans(n_clusters=best_k, random_state=0)
+y_pred = kmeans.fit_predict(X)
+
+#will match predicted labels to true labels
+labels = np.zeros_like(y_pred)
+for i in range(best_k):
+    mask = (y_pred == i)
+    labels[mask] = np.bincount(y_true[mask]).argmax()
+
+accuracy = accuracy_score(y_true, labels)
+print(f'Accuracy: {accuracy}')
+
+mat = confusion_matrix(y_true, labels)
+
+# create confusion matrix for best k
+plt.figure(figsize=(best_k, best_k))
+sns.heatmap(mat, square=True, annot=True, fmt='d', cmap='GnBu', cbar=True,
+            xticklabels=np.unique(y_true),
+            yticklabels=np.unique(y_true))
+
+plt.title('Confusion Matrix for best K')
+plt.xlabel('True label')
+plt.ylabel('predicted label')
+plt.show()
