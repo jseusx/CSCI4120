e097e75b5859dfe3de90ff3f400f71f7f6ecaa17
Jesus <113149095+jseusx@users.noreply.github.com>
2024-10-04 20:11:49 -0400

    Update hw2.py

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -11,9 +11,6 @@ from yellowbrick.cluster import KElbowVisualizer
 X, y_true = make_blobs(n_samples=300, centers=4,
                        cluster_std=0.60, random_state=0)
 
-#plt.scatter(X[:, 0], X[:, 1], s=50)
-# plt.show()
-
 model = KMeans()
 
 visualizer = KElbowVisualizer(model, k=(1,12))
